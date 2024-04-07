c982658e79e93baefbfc16ec68205c0924608266
Abouzar <abouzarmoradian1@gmail.com>
2024-04-06 22:21:34 -0400

    Initial Commit

---
+++ VideoFeed/VideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFeedApp.swift
+//  VideoFeed
+//
+//  Created by Abouzar Moradian on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
